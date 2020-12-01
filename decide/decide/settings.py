be572ac0d6b5d88c348ab773381db4d81304ee5a
javruimun <javruimun@alum.us.es>
2020-12-01 19:19:56 +0100

    Vamos

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -120,6 +120,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'decide',
+        'PASSWORD': 'decide',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
